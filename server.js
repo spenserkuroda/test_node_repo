588d0bf71d28765660ed82fd55680485720fef91
Kuroda <Kuroda.Spenser@con-way.com>
2018-04-24 15:37:40 -0700

    Updated http request

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ http.createServer(function (req, res) {
     res.end();
   });
     
-    // res.writeHead(200, {'Content-Type': 'text/html'});
-    // res.write('Hello World!'); //write a response to the client
-    // res.end(); //end the response
-    // console.log('touched the server');
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(req.url); //write a response to the client
+    res.end(); //end the response
+    console.log('touched the server');
 }).listen(8080); //the server object listens on port 8080
 
 
